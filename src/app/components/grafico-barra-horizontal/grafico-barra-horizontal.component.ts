3e6d083fcc521703a5ad0e39af71078ed2134079
israel <israel.bejarano.a@gmail.com>
2019-12-08 17:44:51 +0100

    generar valores de forma aleatorioa

--- src/app/components/grafico-barra-horizontal/grafico-barra-horizontal.component.ts
+++ src/app/components/grafico-barra-horizontal/grafico-barra-horizontal.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-grafico-barra-horizontal',
   templateUrl: './grafico-barra-horizontal.component.html',
   styleUrls: ['./grafico-barra-horizontal.component.css']
 })
-export class GraficoBarraHorizontalComponent implements OnInit {
+export class GraficoBarraHorizontalComponent implements OnInit, OnDestroy {
 
   results: any[] = [
     {
@@ -38,13 +38,29 @@ export class GraficoBarraHorizontalComponent implements OnInit {
 
   colorScheme = 'nightLights';
 
-  constructor() { }
+  intervalo;
+
+  constructor() {
+    this.intervalo = setInterval(() => {
+      console.log('tick');
+      const newResults = [...this.results];
+
+      // tslint:disable-next-line: forin
+      for (let i in newResults) {
+        newResults[i].value = Math.round(Math.random() * 500);
+      }
+      this.results = [...newResults];
+    }, 1500);
+  }
 
   onSelect(event) {
     console.log(event);
   }
 
   ngOnInit() {
+  }
+  ngOnDestroy(): void {
+    clearInterval(this.intervalo);
   }
 
 }
