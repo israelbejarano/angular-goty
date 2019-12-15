614753fc95a31a30ac44d1a97c7a05e9165fcc9c
israel <israel.bejarano.a@gmail.com>
2019-12-15 14:20:12 +0100

    datos en tiempo real con AngularFirestore

--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { map } from 'rxjs/operators';
+import { Game } from '../../interfaces/interfaces';
 
 @Component({
   selector: 'app-inicio',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
 
-  constructor() { }
+  constructor(private db: AngularFirestore) { }
 
   ngOnInit() {
+    this.db.collection('goty').valueChanges().pipe(
+        map((resp: Game[]) => {
+          // estoy diciendo que del objeto Game solo necesito name y votos y que eso lo voy
+          // a transformar en un objeto con las propiedades name y value: votos.
+          // Esto es posible gracias a la DESTRUCTURACION de arreglos ({}) de js
+          // para pasar de  un objeto Game a otro objeto
+          // name, value que es como lo necesita la grafica para pintar los datos
+          // todo esto gracias al perador pipe y al map de los rxjs
+
+          // modo 'menos claro'
+          // return resp.map(({name, votos}) => ({name, value: votos}));
+
+          // modo 'mas claro'
+          return resp.map( juego => {
+            return {
+              name: juego.name,
+              value: juego.votos
+            };
+          });
+        })).subscribe(resp => {
+      console.log(resp);
+    });
   }
 
 }
